94cf197c9f602c2ab70474099e5ef24c38b3ce21
SAMUEL DE SOUZA TANK PESSOA <21276436@CFP127ALUNO.LOCAL>
2022-04-28 16:59:24 -0300

    start repo

---
+++ app/src/main/java/com/example/games_kotlin_ui_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.games_kotlin_ui_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
