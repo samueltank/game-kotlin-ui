8e601871487203164c30438877b9314b3aefc699
SAMUEL DE SOUZA TANK PESSOA <21276436@CFP127ALUNO.LOCAL>
2022-05-05 16:56:40 -0300

    secund screen done!

--- app/src/main/java/com/example/games_kotlin_ui_layout/MainActivity.kt
+++ app/src/main/java/com/example/games_kotlin_ui_layout/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.games_kotlin_ui_layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.games_kotlin_ui_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
     }
 }
